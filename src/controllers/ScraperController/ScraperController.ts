fb2adca13b5e2df0b28271075fa122a564964d23
Cristian Azócar <caam.1429@gmail.com>
2021-04-07 15:08:56 -0400

    Scrape websites and urls in parallel

--- src/controllers/ScraperController/ScraperController.ts
+++ src/controllers/ScraperController/ScraperController.ts
@@ -1,12 +1,13 @@
-/* eslint-disable no-await-in-loop */
 import { performance } from 'perf_hooks';
 import { Request, Response } from 'express';
 import { IProduct } from 'src/types/interfaces';
 import { Website } from 'src/types/enums';
 import ScraperFactory from 'src/helpers/ScraperFactory';
-// import PaginationBuilder from 'src/helpers/PaginationBuilder';
 import logger from 'src/utils/logger';
+import { asyncForEachParallel } from 'src/utils';
+// import PaginationBuilder from 'src/helpers/PaginationBuilder';
 
+const websites: Website[] = [Website.Zmart];
 export default class ScraperController {
   constructor() {
     this.scrape = this.scrape.bind(this);
@@ -17,20 +18,19 @@ export default class ScraperController {
     // TODO: should we use Redis for something?
     // TODO: scrape all catalogs ("Próximamente", "Usados", etc)
     const allProducts: IProduct[] = [];
-    const websiteKeys: string[] = Object.keys(Website);
     const urls: string[] = [
       'https://www.zmart.cl/scripts/proddisplay_page.asp?id=321&idRowVar=32641&idRow=2997',
       'https://www.zmart.cl/scripts/proddisplay_page.asp?id=361&idRowVar=34809&idRow=3155',
     ];
 
-    // Iterate over all the websites
-    for (let i = 0; i < websiteKeys.length; i++) {
-      const website: Website = Website[websiteKeys[i] as keyof typeof Website];
+    // Scrape all the websites
+    await asyncForEachParallel(websites, async (website: Website) => {
       const scraper = ScraperFactory.getScraper(website);
+      logger.info(`Scraping website ${website}`);
 
-      // Iterate over all the catalogs
-      for (let j = 0; j < urls.length; j++) {
-        scraper.config.url = urls[j];
+      // Scrape all the URLs
+      await asyncForEachParallel(urls, async (url: string) => {
+        scraper.config.url = url;
 
         const t0: number = performance.now();
         const products: IProduct[] = await scraper.scrape();
@@ -38,8 +38,8 @@ export default class ScraperController {
 
         logger.info(`Scraping finished in ${t1 - t0} milliseconds`);
         allProducts.push(...products);
-      }
-    }
+      });
+    });
     // const paginationBuilder = new PaginationBuilder(Website.Zmart);
 
     res.json(allProducts);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,3 +22,20 @@ export async function sleep(millisecondsTimeout: number): Promise<void> {
 export function sanitizeUrl(url: string): string {
   return url.endsWith('?') ? url.substring(0, url.length - 1) : url;
 }
+
+export async function asyncForEach<T>(
+  array: Array<T>,
+  callback: (item: T, index: number) => void
+): Promise<void> {
+  for (let index = 0; index < array.length; index++) {
+    // eslint-disable-next-line no-await-in-loop
+    await callback(array[index], index);
+  }
+}
+
+export async function asyncForEachParallel<T>(
+  array: Array<T>,
+  callback: (item: T, index: number) => void
+): Promise<void> {
+  await Promise.all(array.map(callback));
+}
