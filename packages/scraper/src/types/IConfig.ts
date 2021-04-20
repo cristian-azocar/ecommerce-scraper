74b2e18734b49e2806741003beecc570f5858d40
Cristian Azócar <caam.1429@gmail.com>
2021-04-20 18:42:11 -0400

    Improve code

--- packages/scraper/src/types/IConfig.ts
+++ packages/scraper/src/types/IConfig.ts
@@ -2,13 +2,6 @@ import { Availability, Category, Condition, Retail } from '@project/database';
 
 export default interface IConfig {
   port: number;
-  database: {
-    user: string;
-    password: string;
-    host: string;
-    port: number;
-    database: string;
-  };
   retails: Retail[];
   availabilities: Availability[];
   conditions: Condition[];
