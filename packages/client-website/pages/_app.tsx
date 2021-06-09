3fca475f346885fcb8a695e8ee0c122e3e378f9c
Cristian Azócar <caam.1429@gmail.com>
2021-06-09 15:59:44 -0400

    Import UI library CSS

--- packages/client-website/pages/_app.tsx
+++ packages/client-website/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '@project/ui/src/core/index.scss'; // TODO: import a minified and CSS version
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
