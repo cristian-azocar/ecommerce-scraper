9c3bca80a40eb8a14777366af6d1ac96f94de53b
Cristian Azócar <caam.1429@gmail.com>
2021-06-09 12:40:35 -0400

    Create InputGroup and InputAdornment components

---
+++ packages/ui/src/components/InputAdornment/InputAdornment.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import clsx from 'clsx';
+import './InputAdornment.scss';
+
+export interface InputAdornmentProps
+  extends React.ComponentPropsWithRef<'div'> {
+  position: 'left' | 'right';
+}
+
+function InputAdornment(
+  props: InputAdornmentProps,
+  ref: React.Ref<HTMLDivElement>
+): JSX.Element {
+  const { children, className, position, ...rest } = props;
+  const classes = clsx(
+    'input-adornment',
+    [`input-adornment-${position}`],
+    className
+  );
+
+  return (
+    <div ref={ref} className={classes} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+export default React.forwardRef<HTMLDivElement, InputAdornmentProps>(
+  InputAdornment
+);
