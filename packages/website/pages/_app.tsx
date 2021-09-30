478e9445affca64f4096760549fb5cd0fd0f682d
Cristian Azócar <caam.1429@gmail.com>
2021-09-30 17:57:38 -0300

    Remove redux

--- packages/website/pages/_app.tsx
+++ packages/website/pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css';
-import '@project/ui/src/core/index.scss';
 import type { AppProps } from 'next/app';
+import '@project/ui/src/core/index.scss';
 import Layout from '../components/layout/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
